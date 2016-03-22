3e54c3e1ba84c5778f525488b13e27b0ec25a814
ardessau <ardessau@comcast.net>
2016-03-21 20:45:30 -0600

    made changes to package.json and app.js on 3/21/16

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+var port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+  console.log('Listening on port' + port);
 });
